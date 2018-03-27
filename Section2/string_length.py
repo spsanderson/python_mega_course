3830c35f3d51aef02febf92eb3565b8c153db768
spsanderson <spsanderson@gmail.com>
2018-03-27 14:39:41 -0400

    addition of float check

--- Section2/string_length.py
+++ Section2/string_length.py
@@ -1,7 +1,9 @@
 def string_length(mystring):
     if type(mystring) == int:
         return "Sorry, integers have no length"
+    elif type(mystring) == float:
+        return "Sorry, floats have no length"
     else:
         return len(mystring)
 
-print(string_length("Steve"))    
+print(string_length(1.1))    
