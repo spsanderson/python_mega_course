b4940cdb6bc7947c011c7b3fdd9f14ebd019299e
spsanderson <spsanderson@gmail.com>
2018-03-27 14:44:36 -0400

    update c_to_f for abs zero

--- Section2/celsius_to_fahrenheit.py
+++ Section2/celsius_to_fahrenheit.py
@@ -1,5 +1,8 @@
 def celsius_to_f(deg_c):
-    deg_f = deg_c * (9/5) + 32
-    return deg_f
+    if deg_c < -273.15:
+        return "You are at absolute zero"
+    else:
+        deg_f = deg_c * (9/5) + 32
+        return deg_f
 
-print(celsius_to_f(10))
+print(celsius_to_f(-300))
