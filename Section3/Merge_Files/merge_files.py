1462560505d12b01d2610143e1a6d65a37d552c6
spsanderson <spsanderson@gmail.com>
2018-04-01 09:38:25 -0400

    minor readability change

--- Section3/Merge_Files/merge_files.py
+++ Section3/Merge_Files/merge_files.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 f = glob2.glob("file*.txt")
 with open(datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%F") + ".txt","w") as myfile:
-    for f in f:
-        with open(f,"r") as fname:
+    for file in f:
+        with open(file,"r") as fname:
             myfile.write(fname.read() + "\n")
 
