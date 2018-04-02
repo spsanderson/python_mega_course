631cab64c8f0e8632042790604ff712139c66ff2
spsanderson <spsanderson@gmail.com>
2018-04-02 13:57:12 -0400

    complete interactive dictionary

--- Section5/interactive_dictionary.py
+++ Section5/interactive_dictionary.py
@@ -10,13 +10,31 @@ def definition(word):
     # make all words lower case to match case in data.json
     word = word.lower()
     # make sure the word is in data.json file
+    # if the word is in the data.json file then return the definition
     if word in data:
         # the results come back as a list
         #print(type(data[word]))
         return data[word]
+    # if the word is not found as it is spelt try to find a close approximation and ask if 
+    # that is the word they are looking for
+    elif len(get_close_matches(word,data.keys())) > 0:
+        yn = input("Did you mean %s instead? Y or N " % get_close_matches(word, data.keys())[0])
+        yn = yn.upper()
+        if yn == "Y":
+            word = get_close_matches(word, data.keys())[0]
+            return data[word]
+        elif yn == "N":
+            return "Please search another word we can't find %s " % word
+        else:
+            return "We did not understand your query, please try again"
+    # if all else fails, then tell them the word is not in the dictionary
     else:
-        return "That word is not in the dictionary, please recheck the word"
+        return "%s is not in the dictionary, please recheck the word" % word
     
 word = input("Enter word: ")
-
-print(definition(word))
+output = definition(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
