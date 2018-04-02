eafed4e82f0fc434ab8159522e61d93be15bc194
spsanderson <spsanderson@gmail.com>
2018-04-02 10:29:11 -0400

    simple program using try/except for dealing with errors

---
+++ Section4/errors.py
@@ -0,0 +1,16 @@
+# use the try/except framework to help work with errors
+
+# Get user input
+a = int(input("Enter your numerator: "))
+b = int(input("Enter your denominator: "))
+
+# define function here
+def divide(a, b):
+    try:
+        return a/b
+    except:
+        return "Division by zero is meaningless"
+
+# print result of the function call
+print(divide(a,b))
+print("End of program")
