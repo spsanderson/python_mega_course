0bc3e80ed911fad50abb03e8204347b378dd7da6
spsanderson <spsanderson@gmail.com>
2018-04-03 11:07:35 -0400

    bug fix: allow for names to be looked up without using the "Did you mean..." portion of code.

--- Section5/app1.py
+++ Section5/app1.py
@@ -4,19 +4,22 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(w):
-    w = w.lower()
-    if w in data:
-        return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
-        if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
-        elif yn == "N":
-            return "The word doesn't exist. Please double check it."
-        else:
-            return "We didn't understand your entry."
+    if not str.upper(w[0]):
+        w = w.lower()
     else:
-        return "The word doesn't exist. Please double check it."
+        #w = w.lower()
+        if w in data:
+            return data[w]
+        elif len(get_close_matches(w, data.keys())) > 0:
+            yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+            if yn == "Y":
+                return data[get_close_matches(w, data.keys())[0]]
+            elif yn == "N":
+                return "The word doesn't exist. Please double check it."
+            else:
+                return "We didn't understand your entry."
+        else:
+            return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 output = translate(word)
