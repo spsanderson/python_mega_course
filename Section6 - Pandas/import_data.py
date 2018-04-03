47df0bc17c399bcec2fbb89f601f1a60007e8626
spsanderson <spsanderson@gmail.com>
2018-04-03 13:56:18 -0400

    added some dataframes

--- Section6 - Pandas/import_data.py
+++ Section6 - Pandas/import_data.py
@@ -2,3 +2,9 @@ import pandas
 
 df1 = pandas.DataFrame([[2,4,6],[10,20,30]])
 print(df1)
+
+df1 = pandas.DataFrame([[2,4,6],[10,20,30]], columns=["Price","Age","Value"])
+print(df1)
+
+df2 = pandas.DataFrame([{"Name":"Jack"},{"Name":"Jon"}])
+print(df2)
