88715e8ab8331e7c6a744048d7b1ebe1901a1f67
spsanderson <spsanderson@gmail.com>
2018-04-15 08:53:16 -0400

    make location a var add marker to map

--- Section8 - Creating Webmaps with Python and Folium/app2-web-map/mapping.py
+++ Section8 - Creating Webmaps with Python and Folium/app2-web-map/mapping.py
@@ -1,4 +1,6 @@
 import folium
-
-map = folium.Map(location=[40.766483,-73.0207118], zoom_start=10, tiles="Mapbox Bright")
+loc = [40.766483,-73.0207118]
+map = folium.Map(location=loc, zoom_start=10, tiles="OpenStreetMap")
 map.save("Map1.html")
+folium.Marker(loc,popup="My Office").add_to(map)
+map.save("Map2.html")
