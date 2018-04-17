0a3bb503a74e098909e9e75daed0b9a53f69b575
spsanderson <spsanderson@gmail.com>
2018-04-17 19:36:29 -0400

    add function to color markers based on elevation

--- Section8 - Creating Webmaps with Python and Folium/app2-web-map/volcanoes.py
+++ Section8 - Creating Webmaps with Python and Folium/app2-web-map/volcanoes.py
@@ -10,18 +10,32 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 volname = list(data["NAME"])
 
+# lets make a function to color the map markers based on elevation
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'blue'
+    else:
+        return 'red'
+
 # create base map
 map = folium.Map(location=[38.58, -99.09], zoom_start=5)
 # create feature group
 fg = folium.FeatureGroup(name="My Map of Volcanoes in USA")
 # loop through lat and lon, use zip()
 for lt, ln, el, nm in zip(lat, lon, elev, volname):
-    fg.add_child(folium.Marker(location=([lt, ln])
-    , popup = folium.Popup(
-        "Elevation: " + str(el) + "m - Name: " + str(nm), parse_html=True
+    fg.add_child(
+        folium.Marker(
+            location=([lt, ln])
+            , popup = folium.Popup(
+                "Elevation: " + str(el) + "m - Name: " + str(nm), parse_html=True
+            )
+            , icon = folium.Icon(
+                color = color_producer(el)
+            )
         )
     )
-)
     map.add_child(fg)
 map.save("USA_Volcanoes.html")
 # save map as html file
