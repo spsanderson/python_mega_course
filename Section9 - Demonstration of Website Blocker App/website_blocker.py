2b113315fddc458bff10f2b518ad9fad89663a14
spsanderson <spsanderson@gmail.com>
2018-04-21 21:59:57 -0400

    begining of website blocker uses host from drivers\etc

---
+++ Section9 - Demonstration of Website Blocker App/website_blocker.py
@@ -0,0 +1,11 @@
+# import modules/libraries
+import time
+
+# set variables, host_path, redirect address and website_list
+host_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com", "www.twitter.com", "twitter.com"]
+
+while True:
+    print()
+    time.sleep(1)
