71cfe17d09af04d4b5a841793555534c0ec7a8d0
spsanderson <spsanderson@gmail.com>
2018-04-22 20:47:48 -0400

    initial structure of while loop done

--- Section9 - Demonstration of Website Blocker App/website_blocker.py
+++ Section9 - Demonstration of Website Blocker App/website_blocker.py
@@ -1,5 +1,6 @@
 # import modules/libraries
 import time
+from datetime import datetime as dt
 
 # set variables, host_path, redirect address and website_list
 host_path = r"C:\Windows\System32\drivers\etc\hosts"
@@ -7,5 +8,10 @@ redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com", "www.twitter.com", "twitter.com"]
 
 while True:
-    print()
-    time.sleep(1)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day, 16):
+        print("The time is {} and you should not be at that website".format(dt.now()))
+        time.sleep(5)
+        break
+    else:
+        print("Ok you can go there")
+    time.sleep(5)
