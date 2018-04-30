3485363ac77fdab3d7c26e53de8290853f50d776
spsanderson <spsanderson@gmail.com>
2018-04-30 15:38:20 -0400

    minor modifications, links to homepage and about page

--- Section10 - Building a Website with Python and Flask/steve_flask_site.py
+++ Section10 - Building a Website with Python and Flask/steve_flask_site.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
+# Homepage
 @app.route('/')
 def home():
-    return "Website content goes here"
+    #return "Homepate goes here"
+    return render_template("home.html")
+
+# About page
+@app.route('/about/')
+def about():
+    #return "About page"
+    return render_template("about.html")
 
 if __name__=="__main__":
     app.run(debug=True)
