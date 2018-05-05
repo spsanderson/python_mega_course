66a1ee448389e626c1762e031eb305be4cbc88d4
spsanderson <spsanderson@gmail.com>
2018-05-04 23:19:19 -0400

    add IF NOT EXISTS statement to table creation

--- Section12 - Python for SQLite and PostgreSQL Databases/db_sqlite_postgresql.py
+++ Section12 - Python for SQLite and PostgreSQL Databases/db_sqlite_postgresql.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect("lite.db")
 cur = conn.cursor()
-cur.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
+cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
 conn.commit()
 conn.close()
