14686dbfad3b5cde6fc0202a779a28e4178387e9
spsanderson <spsanderson@gmail.com>
2018-05-04 23:28:20 -0400

    make functions

--- Section12 - Python for SQLite and PostgreSQL Databases/db_sqlite_postgresql.py
+++ Section12 - Python for SQLite and PostgreSQL Databases/db_sqlite_postgresql.py
@@ -1,7 +1,21 @@
 import sqlite3
 
-conn = sqlite3.connect("lite.db")
-cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-conn.commit()
-conn.close()
+def create_table():
+    # Connect to/create database
+    conn = sqlite3.connect("lite.db")
+    # create curson object
+    cur = conn.cursor()
+    # create table
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    # commit changes to db
+    conn.commit()
+    # close connection to db
+    conn.close()
+
+def insert():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    # add data to table
+    cur.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.5)")
+    conn.commit()
+    conn.close()
