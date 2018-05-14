d37b6e5e2c64981ecc79d4e48158400bfd27cef0
spsanderson <spsanderson@gmail.com>
2018-05-14 11:58:17 -0400

    testing backend file

--- Section13 - Building a Desktop Database Application/book_lib_backend.py
+++ Section13 - Building a Desktop Database Application/book_lib_backend.py
@@ -1,1 +1,44 @@
-import sqlite3 as sql
+"""
+The backend script for the book library frontend
+
+"""
+
+import sqlite3
+
+# Create a db / connection to db
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS book"
+        "("
+            "id INTEGER PRIMARY KEY"
+            ", title TEXT"
+            ", author TEXT"
+            ", year INTEGER"
+            ", isbn INTEGER"
+        ")"
+    )
+    conn.commit()
+    conn.close()
+
+# Insert data into db
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ?,?,?,?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    conn.commit()
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+connect()
+insert("Test 1", "Book Worm", 1918, 1234567890)
+print(view())
