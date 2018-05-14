22d8c1a1585f6fea9965f96cc26d31cd993c78d5
spsanderson <spsanderson@gmail.com>
2018-05-14 11:02:38 -0400

    add labels and their corresponding entry boxes

--- Section13 - Building a Desktop Database Application/book_library.py
+++ Section13 - Building a Desktop Database Application/book_library.py
@@ -19,17 +19,29 @@ from tkinter import *
 # Main window
 window = Tk()
 
-# Labels
+# Labels / Entry Box's
 lbl_Title = Label(window, text = "Title:")
 lbl_Title.grid(row = 0, column = 0)
+e_Title = StringVar()
+e_Title = Entry(window, textvariable = e_Title)
+e_Title.grid(row = 0, column = 1)
 
 lbl_Year = Label(window, text = "Year:")
 lbl_Year.grid(row = 1, column = 0)
+e_Year = StringVar()
+e_Year = Entry(window, textvariable = e_Year)
+e_Year.grid(row = 1, column = 1)
 
 lbl_Author = Label(window, text = "Author:")
 lbl_Author.grid(row = 0, column = 2)
+e_Author = StringVar()
+e_Author = Entry(window, textvariable = e_Author)
+e_Author.grid(row = 0, column = 3)
 
 lbl_ISBN = Label(window, text = "ISBN:")
 lbl_ISBN.grid(row = 1, column = 2)
+e_ISBN = StringVar()
+e_ISBN = Entry(window, textvariable = e_ISBN)
+e_ISBN.grid(row = 1, column = 3)
 
 window.mainloop()
